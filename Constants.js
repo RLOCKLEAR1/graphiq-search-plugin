958b39cbe4c35c3bfb06fa767af4bf1afb609ca7
David Schnurr <dschnurr@findthebest.com>
2015-08-21 16:52:35 -0700

    Versioning

--- Constants.js
+++ Constants.js
@@ -8,7 +8,7 @@ module.exports = {
 	PROTOCOL: PROTOCOL,
 	HOST: HOST,
 	ICON: '//img1.findthebest.com/sites/default/files/4261/media/images/_6326290.png',
-	VERSION: 0.1,
+	VERSION: '0.1.0',
 	MODE_MODAL: 'modal',
 	MODE_SIDEBAR: 'sidebar',
 	MODE_CONTAINER: 'container'
