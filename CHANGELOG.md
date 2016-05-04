b95f491884dfaeb48217fbf7f946992e53fa293f
David Schnurr <schnurr@gmail.com>
2016-05-03 17:32:58 -0700

    1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+##v1.0.2
+
+- Fix the 'container' mode
+
+##v1.0.1
+
+- Update placeholder image path, minor internal refactoring
+
 ##v1.0.0
 
 - Rebrand as Graphiq Search plugin, change globals and plugin references
--- src/Constants.js
+++ src/Constants.js
@@ -1,7 +1,7 @@
 module.exports = {
 	HOST: 'https://www.graphiq.com',
 	ICON: 'https://img1.graphiq.com/sites/default/files/4261/media/images/_6326290.png',
-	VERSION: '1.0.1',
+	VERSION: '1.0.2',
 	MODE_MODAL: 'modal',
 	MODE_SIDEBAR: 'sidebar',
 	MODE_CONTAINER: 'container'
